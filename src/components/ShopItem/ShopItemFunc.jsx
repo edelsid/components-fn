606da7259524533e38a684a85d397841d844b04f
Xenia Ch <eradonna@yandex.ru>
2023-12-07 06:08:37 +0300

    initial commit

---
+++ src/components/ShopItem/ShopItemFunc.jsx
@@ -0,0 +1,30 @@
+export const ShopItemFunc = (props) => {
+   const { item } = props;
+   return (
+      <div className="main-content">
+        <h2>{item.brand}</h2>
+        <h1>{item.title}</h1>
+        <h3>{item.description}</h3>
+        <div className="description">
+          {item.descriptionFull}
+        </div>
+        <div className="highlight-window mobile">
+          <div className="highlight-overlay"></div>
+        </div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+        <div className="price">{item.currency}{zeroes(item.price)}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+    )
+  }
+
+function zeroes (price) {
+   let newPrice = price;
+   const values = price.toString().split('.');
+   if (values.length === 1) {
+      newPrice = price.toFixed(2);
+   }
+   return newPrice;
+}
